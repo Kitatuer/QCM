2be7e5d5306ede9ea747b87606e50f5e08f39eae
Kitatuer <quent191150@gmail.com>
2026-05-24 16:40:29 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.QCM_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCM2LqtE-i5Kuy9nLV__bholfujLnoHryw",
-  authDomain: "qcm-guilde-double-creme.firebaseapp.com",
-  projectId: "qcm-guilde-double-creme",
-  storageBucket: "qcm-guilde-double-creme.firebasestorage.app",
-  messagingSenderId: "783299359160",
-  appId: "1:783299359160:web:ea2538e843138593421f4d"
-};
